484642d0ef67bd2839f7bbd9b20343adf475bba1
unknown <phamquocduy3112003@gmail.com>
2026-05-21 17:05:11 +0700

    initial commit

---
+++ src/main/java/com/app/wavelength/WavelengthApplication.java
@@ -0,0 +1,13 @@
+package com.app.wavelength;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WavelengthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WavelengthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/wavelength/WavelengthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.wavelength;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WavelengthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
